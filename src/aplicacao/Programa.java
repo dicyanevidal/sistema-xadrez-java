2351d54d36cb92ef742f9659d3a54ca300bcead8
dicyanevidal <dicyanevidal@hotmail.com>
2021-07-06 21:27:48 -0300

    aplicacao do tabuleiro

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,14 +1,12 @@
 package aplicacao;
 
-import tabuleiro.Posicao;
+import tabuleiro.Tabuleiro;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Posicao posicao = new Posicao(5, 3);
-		System.out.println(posicao);
-
+		Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 	}
 
 }
